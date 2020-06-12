75122503759500f9710a2db43748b221777f5ab9
EdwinMLara <emlara35@gmail.com>
2020-06-12 00:38:47 -0500

    se modifico la clase para utilizar los metodos de la interface de crud

--- src/main/java/com/example/demo/LocationService.java
+++ src/main/java/com/example/demo/LocationService.java
@@ -3,40 +3,36 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service    
+@Service
 public class LocationService {
-    Location l1 = new Location("l1","lagos");
-    Location l2 = new Location("l2","Abasa");
-    
-    List<Location> locations = Arrays.asList(l1,l2);
-    List<Location> locations2 = new ArrayList<>(locations);
 
-    public List<Location>getAllLocatios(){
-        return this.locations2;
+    @Autowired
+    private LocationRepositoy locationrespository;
+
+    public List<Location> getAllLocatios() {
+        List<Location> locations = new ArrayList<>();
+        locationrespository.findAll().forEach(locations::add);
+        return locations;
     }
 
-    public Location getLocation(String id){
-        Location location = locations2.stream().filter(l -> id.equals(l.getId())).findFirst().orElse(null);
-        return location;
+    public Optional<Location> getLocation(String id) {
+        return locationrespository.findById(id);
     }
 
     public void addLocation(Location location){
-        locations2.add(location);
+        locationrespository.save(location);
     }
 
     public void updateLocation(String id,Location location){
-        for(int i=0;i<locations2.size();i++){
-            Location aux = locations2.get(i);
-            if(aux.getId().equals(id)){
-                locations2.set(i, location);
-                break;
-            }
-        }
+        locationrespository.save(location);
     }
 
     public void deleteLocation(String id){
-        locations2.removeIf(l -> l.getId().equals(id));
+        locationrespository.deleteById(id);
     }
 }
