85f0926e4133b2dc7d1c40317e9bc206f3525e78
EdwinMLara <emlara35@gmail.com>
2020-06-12 23:46:53 -0500

    Se agreron algunos metodos para ver como utilizar las consultas personalizadas

--- src/main/java/com/example/demo/StudentRepository.java
+++ src/main/java/com/example/demo/StudentRepository.java
@@ -1,7 +1,25 @@
 package com.example.demo;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import java.util.Optional;
 
 public interface StudentRepository extends CrudRepository<Student,String>{
+    /**Se agregan algunos ejemplos para observar como funcinan las consultas en
+     * jpa obteniendo diferentes resultados
+     */
+
+    @Query("SELECT t.name FROM Student t where t.id = :id")
+    public Optional<String> findNameById(@Param("id") String id);
+
+    @Query("SELECT * FROM Student t where t.id = :id")
+    public Optional<Student> findById(@Param("id")String id);
+
+    @Query("SELECT * FROM Student t where t.id = ?1 AND t.department = ?2")
+    public Optional<Student> findByNameAndDepartment(String name, String department);
+
+    @Query("SELECT * FROM Student t where t.name = %?1%")
+    public Optional<Student> findByCriteria(String criteria);
     
 }
